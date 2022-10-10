09ad6254a12ffa1102ab9c94026cd4fcd597c8e7
Jordi Vila <jordi.vilacast@gmail.com>
2022-10-10 19:44:35 +0200

    [FEAT] Declare components and import forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { WhoAmIComponent } from './pages/landing/who-am-i/who-am-i.component';
+import { RoadmapComponent } from './pages/landing/roadmap/roadmap.component';
+import { ContactComponent } from './pages/landing/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    WhoAmIComponent,
+    RoadmapComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
