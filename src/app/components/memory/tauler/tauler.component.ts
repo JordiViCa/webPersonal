9cb7d92a51384c4d131f7adc3ddc1dff4be6762b
Jordi Vila <jordi.vilacast@gmail.com>
2022-10-11 18:58:10 +0200

    [FEAT] Create tauler

---
+++ src/app/components/memory/tauler/tauler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tauler',
+  templateUrl: './tauler.component.html',
+  styleUrls: ['./tauler.component.scss']
+})
+export class TaulerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
